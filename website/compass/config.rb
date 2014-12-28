af09efb2319c7f5ac2c5861c7eb21d105d0996b5
colm <mail@colm.be>
2014-12-28 13:31:11 +0100

    this should be most of the necessary structure, excluding browser sync through gulp which is still causing trouble and incompatibilities between 'node-sass' and 'gulp-sass'

--- website/compass/config.rb
+++ website/compass/config.rb
@@ -11,8 +11,8 @@ css_dir = "assets/css"
 sass_dir = "assets/css"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
-#svg_dir = "assets/svg"
-#fonts_dir = "assets/fonts"
+svg_dir = "assets/svg"
+fonts_dir = "assets/fonts"
 
 # Output style and comments
 # -----------------------------------------------------------------------------
@@ -20,11 +20,9 @@ javascripts_dir = "assets/js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 # Over-ride with force compile to change output style with: compass compile --output-style compressed --force
-output_style = :expanded
-
+output_style = :compressed
 line_comments = false
 cache = true
-color_output = false # required for Mixture
 
 require 'sass-globbing'
 
