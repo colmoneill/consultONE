f52f7a47bd31c183b65a5586dd164835ef6cea9c
colm <mail@colm.be>
2014-12-28 12:36:33 +0100

    Near fully functionnal (reduced) gulp process and correction of all the import url structure left by Sassaparilla Adding in package.json for npm dependencies gitignoring /node_modules

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,79 +1,22 @@
-/*!
-* gulp
-* $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
-*/
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+var minifyCSS = require('gulp-minify-css');
 
-// Load plugins
-var gulp = require('gulp'),
-sass = require('gulp-ruby-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-minifycss = require('gulp-minify-css'),
-uglify = require('gulp-uglify'),
-imagemin = require('gulp-imagemin'),
-rename = require('gulp-rename'),
-concat = require('gulp-concat'),
-notify = require('gulp-notify'),
-cache = require('gulp-cache'),
-livereload = require('gulp-livereload'),
-del = require('del');
-
-// Styles
-gulp.task('styles', function() {
-return gulp.src('src/styles/main.scss')
-.pipe(sass({ style: 'expanded', }))
-.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-.pipe(gulp.dest('dist/styles'))
-.pipe(rename({ suffix: '.min' }))
-.pipe(minifycss())
-.pipe(gulp.dest('dist/styles'))
-.pipe(notify({ message: 'Styles task complete' }));
+gulp.task('compass', function() {
+    gulp.src('./assets/css/*.scss')
+        .pipe(compass({
+            config_file: './compass/config.rb',
+            sass: 'assets/css',
+            css: 'assets/css',
+        }))
+        .on('error', function(err) {
+            console.log(err)
+        })
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./assets/css/'));
+});
+gulp.task('watch', function () {
+    gulp.watch(['./assets/css/**/*.scss'], ['compass']);
 });
 
-// Scripts
-gulp.task('scripts', function() {
-return gulp.src('src/scripts/**/*.js')
-.pipe(concat('main.js'))
-.pipe(gulp.dest('dist/scripts'))
-.pipe(rename({ suffix: '.min' }))
-.pipe(uglify())
-.pipe(gulp.dest('dist/scripts'))
-.pipe(notify({ message: 'Scripts task complete' }));
-});
-
-// Images
-gulp.task('images', function() {
-return gulp.src('src/images/**/*')
-.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-.pipe(gulp.dest('dist/images'))
-.pipe(notify({ message: 'Images task complete' }));
-});
-
-// Clean
-gulp.task('clean', function(cb) {
-del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
-});
-
-// Default task
-gulp.task('default', ['clean'], function() {
-gulp.start('styles', 'scripts', 'images');
-});
-
-// Watch
-gulp.task('watch', function() {
-
-// Watch .scss files
-gulp.watch('src/styles/**/*.scss', ['styles']);
-
-// Watch .js files
-gulp.watch('src/scripts/**/*.js', ['scripts']);
-
-// Watch image files
-gulp.watch('src/images/**/*', ['images']);
-
-// Create LiveReload server
-livereload.listen();
-
-// Watch any files in dist/, reload on change
-gulp.watch(['dist/**']).on('change', livereload.changed);
-
-});
+gulp.task('default', ['compass', 'watch']);
