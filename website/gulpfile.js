912be35d86b27c1d12226b7bc462319e952d199a
colm <mail@colm.be>
2014-12-29 18:43:04 +0100

    automatic height and floats + mobile typographic adjustments

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var compass = require('gulp-compass');
 var minifyCSS = require('gulp-minify-css');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-
 
 gulp.task('compass', function() {
     gulp.src('./assets/css/*.scss')
@@ -18,29 +15,10 @@ gulp.task('compass', function() {
         .pipe(minifyCSS())
         .pipe(gulp.dest('./assets/css/'));
 });
+
+
 gulp.task('watch', function () {
     gulp.watch(['./assets/css/**/*.scss'], ['compass']);
 });
 
-// browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-// Sass task, will run when any SCSS files change & BrowserSync
-// will auto-update browsers
-gulp.task('sass', function () {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'))
-        .pipe(reload({stream:true}));
-});
-
-// Default task to be run with `gulp`
-gulp.task('default', ['watch', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-});
+gulp.task('default', ['compass', 'watch']);
